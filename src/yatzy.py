3d5321ac26a672752bf73f746c60294383af38ce
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-01-03 21:55:19 +0100

    refactor(Se elimino el contructor de la clase Yatzy): Se elimino el constructor de la Clase Yatzy

--- src/yatzy.py
+++ src/yatzy.py
@@ -221,12 +221,4 @@ class Yatzy:
             if counts[i] == 5:
                 return 50
         return 0
-    
 
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
