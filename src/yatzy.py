ad5114d38b5cfb13efcbcd095e16c32d2ae631f6
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-01-04 14:43:18 +0100

    docs: Docs on Class Yatzy and their methods

--- src/yatzy.py
+++ src/yatzy.py
@@ -1,11 +1,19 @@
 class Yatzy:
+    """Class Yatzy: This class consist of a serie of methods that let us to play the Yatzy's game
+    """
     
+    #* Constructor Class Yatzy
     def __init__(self, *dices):
         self.dices = dices
         
-        
+    #* Static Method ones()
     @staticmethod
     def ones(*dices):
+        """ones(): scores the sum of the dice 1
+
+        Returns:
+            int: sum of the dice 1
+        """
         sum_ones = 0
         
         for dice in dices:
@@ -17,6 +25,11 @@ class Yatzy:
     
     @staticmethod
     def twos(*dices):
+        """twos(): scores the sum of the dice 2
+
+        Returns:
+            int: sum of the dice 2
+        """
         sum_twos = 0
         
         for dice in dices:
@@ -26,6 +39,11 @@ class Yatzy:
 
     @staticmethod
     def threes(*dices):
+        """threes(): scores the sum of the dice 3
+
+        Returns:
+            int: sum of the dice 3
+        """
         sum_three = 0
         
         for dice in dices:
@@ -35,6 +53,11 @@ class Yatzy:
     
 
     def fours(self):
+        """fours(): scores the sum of the dice 4
+
+        Returns:
+            int: sum of the dice 4
+        """
         sum_fours = 0
         
         for dice in self.dices:
@@ -45,6 +68,11 @@ class Yatzy:
     
 
     def fives(self):
+        """fives(): scores the sum of the dice 5
+
+        Returns:
+            int: sum of the dice 5
+        """
         sum_fives = 0
         
         for dice in self.dices:
@@ -54,6 +82,11 @@ class Yatzy:
     
 
     def sixes(self):
+        """sixes(): scores the sum of the dice 6
+
+        Returns:
+            int: sum of the dice 6
+        """
         sum_sixes = 0
         
         for dice in self.dices:
@@ -63,7 +96,11 @@ class Yatzy:
     
     @staticmethod
     def score_pair(*dices):
-        
+        """score_pair(): scores the sum of the two highest matching dice
+
+        Returns:
+            int: sum of the two highest matching dice
+        """
         score = 0
         dices = sorted(dices, reverse=True)
         
@@ -79,7 +116,12 @@ class Yatzy:
 
     @staticmethod
     def two_pair(*dices):
-        
+        """two_pair(): If there are two pairs of dice with the same number, the
+        player scores the sum of these dice.
+
+        Returns:
+            int: scores the sum of these dice
+        """
         dices = sorted(dices, reverse=True)
         score = 0
         total_two_pair = 4
@@ -96,7 +138,11 @@ class Yatzy:
         
     @staticmethod
     def three_of_a_kind(*dices):
-        
+        """three_of_a_kind(): If there are three dice with the same number, the player scores the sum of these dice. 
+
+        Returns:
+            int: scores the sum of these dice
+        """
         score = 0
         dices = sorted(dices, reverse=True)
         for dice in tuple(dices):
@@ -111,7 +157,11 @@ class Yatzy:
     
     @staticmethod
     def four_of_a_kind(*dices):
-        
+        """four_of_a_kind(): If there are four dice with the same number, the player scores the sum of these dice. 
+
+        Returns:
+            int: scores the sum of these dice
+        """
         score = 0
         dices = sorted(dices, reverse=True)
         for dice in tuple(dices):
@@ -127,6 +177,11 @@ class Yatzy:
 
     @staticmethod
     def small_straight(*dices):
+        """small_straight(): Let us read if the dices are a small_straight: 1,2,3,4,5
+
+        Returns:
+            int: scores the sum of these dice
+        """
         straight = 15
         score = 0
         
@@ -142,6 +197,11 @@ class Yatzy:
 
     @staticmethod
     def large_straight(*dices):
+        """large_straight(): Let us read if the dices are a large_straight: 2,3,4,5,6
+
+        Returns:
+            int: scores the sum of these dice
+        """
         straight = 20
         score = 0
         
@@ -157,6 +217,11 @@ class Yatzy:
 
     @staticmethod
     def full_house(*dices):
+        """full_house(): Let us read If the dice are two of a kind and three of a kind, the player scores the sum of all the dice. 
+
+        Returns:
+            int: scores the sum of these dice
+        """
         dices = sorted(dices, reverse=True)
         score = 0
         full_house = 5
@@ -172,6 +237,11 @@ class Yatzy:
     
     @staticmethod
     def chance(*dices):
+        """chance(): Let us read If The player scores the sum of all dice, no matter what they read.
+
+        Returns:
+            int: scores the sum of these dice
+        """
         total = 0
         for dice in dices:
             total += dice
@@ -179,6 +249,11 @@ class Yatzy:
 
     @staticmethod
     def yatzy(*dices):
+        """yatzy(): Let us read If all dice have the same number
+
+        Returns:
+            int: scores 50
+        """
         score = 0
         if dices.count(dices[0]) == 5:
             score = 50
@@ -186,7 +261,3 @@ class Yatzy:
             return score
 
         return score
-
-
-if __name__ == "__main__":
-    print(Yatzy.full_house(6,6,5,5,6))
