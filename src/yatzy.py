f78a8ecb968423ae55c1168f16cda071b3506f7a
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-01-04 03:06:59 +0100

    refactor: Refactor on three_of_a_kind() method

--- src/yatzy.py
+++ src/yatzy.py
@@ -93,17 +93,20 @@ class Yatzy:
         return score
         
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
+    def three_of_a_kind(*dices):
+        
+        score = 0
+        dices = list(dices)
+        dices.sort(reverse=True)
+        for dice in tuple(dices):
+            if dices.count(dice) >= 3:
+                score = dice*3
+                break
+            else:
+                continue
+                
+        return score
+    
     
     @staticmethod
     def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
@@ -206,3 +209,4 @@ class Yatzy:
             if counts[i] == 5:
                 return 50
         return 0
+
